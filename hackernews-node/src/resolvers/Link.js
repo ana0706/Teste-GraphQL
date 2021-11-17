ca199833e080dc535bb36739978f0f059f36b7bf
Ana Caroline Vieira <ana.vieira@vnator.com>
2021-11-17 15:25:38 -0300

    first commit

---
+++ hackernews-node/src/resolvers/Link.js
@@ -0,0 +1,12 @@
+function postedBy(parent, args, context) {
+    return context.prisma.link.findUnique({ where: { id: parent.id } }).postedBy()
+  }
+  
+  module.exports = {
+    postedBy,
+    votes,c
+}
+
+function votes(parent, args, context) {
+    return context.prisma.link.findUnique({ where: { id: parent.id } }).votes()
+  }
